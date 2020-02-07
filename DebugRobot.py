28a4a76d5602a1ac5b97d6117a59f32e318f7780
Saha,Sounak <Sounak.Saha@misys.com>
2020-02-07 23:30:46 +0530

    updated

---
+++ DebugRobot.py
@@ -0,0 +1,12 @@
+import sys
+
+# sys.path.append('D:\\UTF\\UnifiedTestFramework\\Libraries\\REST\\HttpLibraryExtended')
+# sys.path.append('D:\\UTF\\UnifiedTestFramework\\Libraries\\Common')
+sys.path.append('D:\\test2\\test11')
+
+# from GenericLib import GenericLib
+# from Parallel import Parallel
+from robot import run
+
+run('D:\\test2\\test11\\Test.robot')
+# run('D:\\UTF\\UTF\\SampleUTFProject\\TestCases\\TACOE_Issues\\test.robot')
